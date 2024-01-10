de763f5f4b84876524e7f5f9c0ffcf423be0b8fd
healer <879403498@qq.com>
2024-01-10 16:27:40 +0800

    update

---
+++ build/CMakeFiles/Code.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Code.dir/src/main.cpp.obj"
+  "CMakeFiles/Code.dir/src/main.cpp.obj.d"
+  "Code.exe"
+  "Code.exe.manifest"
+  "Code.pdb"
+  "libCode.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Code.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
