b919274a9ccc48723843641e7fc22e84e545cbfd
healer <879403498@qq.com>
2024-01-12 00:47:22 +0800

    commit

--- build/CMakeFiles/Code.dir/cmake_clean.cmake
+++ build/CMakeFiles/Code.dir/cmake_clean.cmake
@@ -1,10 +1,10 @@
 file(REMOVE_RECURSE
   "CMakeFiles/Code.dir/src/main.cpp.obj"
   "CMakeFiles/Code.dir/src/main.cpp.obj.d"
-  "Code.exe"
-  "Code.exe.manifest"
-  "Code.pdb"
-  "libCode.dll.a"
+  "D:/C++Code/outFile/Code.exe"
+  "D:/C++Code/outFile/Code.exe.manifest"
+  "D:/C++Code/outFile/Code.pdb"
+  "D:/C++Code/outFile/libCode.dll.a"
 )
 
 # Per-language clean rules from dependency scanning.
