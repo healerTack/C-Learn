1d046aeda92a6e7af5cd6e48d62f16431f811bab
healer <879403498@qq.com>
2024-01-13 00:19:54 +0800

    commit

---
+++ learn_project/build/CMakeFiles/Code.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Code.dir/main.cpp.obj"
+  "CMakeFiles/Code.dir/main.cpp.obj.d"
+  "D:/C++Code/learn_project/Code.exe"
+  "D:/C++Code/learn_project/Code.exe.manifest"
+  "D:/C++Code/learn_project/Code.pdb"
+  "D:/C++Code/learn_project/libCode.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Code.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ standardexample/build/CMakeFiles/app_static.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/app_static.dir/main.cpp.obj"
+  "CMakeFiles/app_static.dir/main.cpp.obj.d"
+  "D:/CMakeLearn/standardexample/app_static.exe"
+  "D:/CMakeLearn/standardexample/app_static.exe.manifest"
+  "D:/CMakeLearn/standardexample/app_static.pdb"
+  "D:/CMakeLearn/standardexample/libapp_static.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/app_static.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
