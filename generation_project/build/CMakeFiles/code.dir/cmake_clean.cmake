52850898ab6be8a71a7bd1c73170259850bae24f
healer <879403498@qq.com>
2024-01-18 17:29:40 +0800

    commit

---
+++ generation_project/build/CMakeFiles/code.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/code.dir/main.cpp.obj"
+  "CMakeFiles/code.dir/main.cpp.obj.d"
+  "D:/C++Code/generation_project/code.exe"
+  "D:/C++Code/generation_project/code.exe.manifest"
+  "D:/C++Code/generation_project/code.pdb"
+  "D:/C++Code/generation_project/libcode.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/code.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- generation_project/main.cpp
+++ generation_project/main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 using namespace std;
 
+inline double square(double x, double y) {return x*x + y*y;}
+
 int main(int argc, char const * argv[]){
-    
+    double x, y;
+    cin >> x >> y;
+    cout << square(x, y) << endl;    
     return 0;
 }
